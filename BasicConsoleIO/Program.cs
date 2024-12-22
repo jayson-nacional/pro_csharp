26bdd5a5deadff651185bc460684e66283c163c4
jayson-nacional <jayson.o.nacional@gmail.com>
2024-12-22 11:41:06 +0800

    Basic input and output

---
+++ BasicConsoleIO/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BasicConsoleIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Basic Console I/O *****");
+		GetUserData();
+    }
+
+	static void GetUserData()
+	{
+		// Get name and age
+		Console.Write("Please enter your name: ");
+		string? userName = Console.ReadLine();
+		Console.Write("Please enter your age: ");
+		string? userAge = Console.ReadLine();
+
+		// Change color
+		ConsoleColor prevColor = Console.ForegroundColor;
+		Console.ForegroundColor = ConsoleColor.Yellow;
+
+		// Echo
+		Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+
+		// Restore previous color
+		Console.ForegroundColor = prevColor;
+	}
+}
--- SimpleCSharpApp/Program.cs
+++ SimpleCSharpApp/Program.cs
@@ -4,6 +4,18 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+		ShowEnvironmentDetails();
+    }
+
+    static void ShowEnvironmentDetails()
+    {
+        foreach (string drive in Environment.GetLogicalDrives())
+        {
+            Console.WriteLine("Drive: {0}", drive);
+        }
+
+        Console.WriteLine("OS: {0}", Environment.OSVersion);
+        Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+        Console.WriteLine(".NET Core Version: {0}", Environment.Version);
     }
 }
